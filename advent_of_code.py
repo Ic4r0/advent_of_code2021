9e68c639995d0c681e31b1692b2b58ad5ad349ef
Ic4r0 <nicola@klee.eu>
2021-12-04 15:34:54 +0100

    Add 4th day solution

--- advent_of_code.py
+++ advent_of_code.py
@@ -16,6 +16,7 @@ from utils.validators import check_valid_arguments
 from days.day_1 import day_1
 from days.day_2 import day_2
 from days.day_3 import day_3
+from days.day_4 import day_4
 
 
 # module
@@ -32,6 +33,8 @@ def save_xmas(selected_day: int, selected_part: int = None, is_test: bool = Fals
         day_2(selected_part, is_test)
     elif selected_day == 3:
         day_3(selected_part, is_test)
+    elif selected_day == 4:
+        day_4(selected_part, is_test)
     elif 0 < selected_day < 26:
         print('No available solution for the selected day')
     else:
