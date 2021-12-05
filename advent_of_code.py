671787be1d8f5e0185c17705cbe467028cb06e53
Ic4r0 <nicola@klee.eu>
2021-12-05 11:33:59 +0100

    Add 5th day solution

--- advent_of_code.py
+++ advent_of_code.py
@@ -17,6 +17,7 @@ from days.day_1 import day_1
 from days.day_2 import day_2
 from days.day_3 import day_3
 from days.day_4 import day_4
+from days.day_5 import day_5
 
 
 # module
@@ -35,6 +36,8 @@ def save_xmas(selected_day: int, selected_part: int = None, is_test: bool = Fals
         day_3(selected_part, is_test)
     elif selected_day == 4:
         day_4(selected_part, is_test)
+    elif selected_day == 5:
+        day_5(selected_part, is_test)
     elif 0 < selected_day < 26:
         print('No available solution for the selected day')
     else:
